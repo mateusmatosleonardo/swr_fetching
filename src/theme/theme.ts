cc9d83caf8a00e90a8b716957295bc1b438011f4
Mateus Leonardo <m4teusmatos@gmail.com>
2023-02-02 17:02:21 -0300

    feat: creating global theme

---
+++ src/theme/theme.ts
@@ -0,0 +1,23 @@
+export const theme = {
+  name: 'DefaultTheme',
+  colors: {
+    WHITE: '#fafafa',
+    BLACK: '#1f1e1f',
+    GRAY: '#908e90',
+    OTHER_GRAY: '#DEDEDE',
+    PURPLE: '#5c5cfc',
+  },
+  fonts: {
+    Rubik_Regular: 'Rubik_400Regular',
+    Rubik_Medium: 'Rubik_500Medium',
+    Rubik_SemiBold: 'Rubik_600SemiBold',
+    Rubik_Bold: 'Rubik_700Bold',
+  },
+  sizes: {
+    SMALL: 12,
+    NORMAL: 14,
+    MEDIUM_LARGE: 16,
+    LARGE: 20,
+    ULTRA_LARGE: 22,
+  },
+};
